a56bfb6ccfff4e9680528499f8b2d957fcc9d54a
robertosegundoa <robertosanttos392@gmail.com>
2021-08-25 20:40:24 -0300

    Create questao05.java

---
+++ questao05.java
@@ -0,0 +1,18 @@
+public class questao05{
+
+     
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++){
+            System.out.println("\n************************************************** \n");
+            System.out.println("**************************************************\n");
+            System.out.println("tabuada de " + i + ": \n");
+            for (int j = 1; j <= 10; j++){
+                System.out.println(i + " x " + j + " = " + (i * j));
+            
+            }
+        }
+    }
+}
+    
+    
+
